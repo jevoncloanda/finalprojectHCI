abdaf4c13508350f8c17497e1a80070dc5fd1ab9
jevoncloanda <jevon.loanda@gmail.com>
2022-06-11 14:20:53 +0700

    added form validation (not yet finished)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// NAVBAR MOBILE MENU
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar_menu');
 
@@ -7,3 +8,31 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu);
+
+// SUBSCRIBE VALIDATION
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const submit = document.getElementById('phone');
+const country = document.getElementById('country');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+    let messages = [];
+    if(name.value.trim() === '' || name.value == null) {
+        messages.push('Name is required');
+    }
+    if(email.value.trim() === '' || email.value == null) {
+        messages.push('Email is required');
+    }
+    if(phone.value.trim() === '' || phone.value == null) {
+        messages.push('Phone Number is required');
+    }
+    if(country.value.trim() === '' || country.value == null) {
+        messages.push('Country is required');
+    }
+
+    if(messages.length > 0) {
+        e.preventDefault();
+        alert(messages.join('\n'));
+    }
+});
